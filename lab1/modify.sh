fca3bf148937ab5f8ac7ea68577cd1ec68e0e3ee
Michał Szopiński <mszopinski@mssnt.pl>
2021-03-28 00:16:57 +0100

    Implement basic case change functionality

--- lab1/modify.sh
+++ lab1/modify.sh
@@ -39,3 +39,42 @@ do
 			;;
 	esac
 done
+
+# ensure all specified files/directories exist
+shift "$((OPTIND-1))"
+for argument in "$@"
+do
+	if [ ! -e $argument ]; then
+		echo "File $argument doesn't exist" >&2
+		exit 2
+	fi
+done
+
+# parse arguments
+for argument in "$@"
+do
+	if [ -d $argument ]	&& $recursive; then
+		echo "parsing directory recursively"
+		continue
+	fi
+	
+	# split argument into path and name
+	basename="$(basename $argument)"
+	dirname="$(dirname $argument)"
+	
+	# modify name according to mode
+	case $mode in
+		lowercase)
+			basename="$(echo $basename | tr [:upper:] [:lower:])"
+			;;
+		uppercase)
+			basename="$(echo $basename | tr [:lower:] [:upper:])"
+			;;
+		sed)
+			echo "not implemented"
+			;;
+	esac
+	
+	# implement change, don't overwrite files and don't move into directories
+	mv -nT "$argument" "$dirname/$basename"
+done
