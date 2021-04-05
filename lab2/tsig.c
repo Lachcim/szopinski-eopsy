993bc5814102588bc0d4df1a68dfd2d0b508154f
Michał Szopiński <mszopinski@mssnt.pl>
2021-04-05 16:01:02 +0200

    Complete first part of lab 2

---
+++ lab2/tsig.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+#define NUM_CHILD 7
+
+pid_t childPids[NUM_CHILD];
+
+int main() {
+	//fork the defined number of processes
+	for (int i = 0; i < NUM_CHILD; i++) {
+		//fork and get pid of child
+		pid_t childPid = fork();
+		childPids[i] = childPid;
+		
+		//algorithm for the child process
+		if (childPid == 0) {
+			printf("Child process %d born, my parent is %d\n", getpid(), getppid());
+			sleep(10);
+			
+			printf("Child process %d signing out\n", getpid());
+			return 0;
+		}
+		
+		//terminate living children on fork failure
+		if (childPid == -1) {
+			fprintf(stderr, "Child #%d failed to spawn, aborting\n", i);
+			
+			for (int j = 0; j < i; j++)
+				kill(childPids[j], SIGTERM);
+			
+			return 1;
+		}
+		
+		//insert delay between forks
+		if (i != NUM_CHILD - 1)
+			sleep(1);
+	}
+	
+	printf("All child processes forked\n");
+	
+	//wait for children to terminate
+	int terminations = 0;
+	while (wait(NULL) != -1)
+		terminations++;
+	
+	printf("All %d child processes terminated\n", terminations);
+}
