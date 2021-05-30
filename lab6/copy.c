c0d879a0aeec79c2ea88a690a9ab430dfbfc69bb
Michał Szopiński <mszopinski@mssnt.pl>
2021-05-30 16:48:26 +0200

    Implement mmap copying

--- lab6/copy.c
+++ lab6/copy.c
@@ -1,8 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/mman.h>
 
 int copyReadWrite(int, int);
 int copyMmap(int, int);
@@ -13,7 +16,10 @@ const char* const ERROR_MESSAGES[] = {
 	"Failed to open the source file\n",
 	"Failed to open the destination file\n",
 	"Failed to read from the source file\n",
-	"Failed to write to the destination file\n"
+	"Failed to write to the destination file\n",
+	"Error while seeking source file\n",
+	"Failed to map source file\n",
+	"Failed to map destination file\n"
 };
 
 int main(int argc, char** argv) {
@@ -46,7 +52,7 @@ int main(int argc, char** argv) {
 	//open source and destination files
 	int srcFd = open(argv[optind], O_RDONLY);
 	if (srcFd == -1) return 2;
-	int destFd = open(argv[optind + 1], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	int destFd = open(argv[optind + 1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 	if (destFd == -1) return 3;
 	
 	//call the proper copying subroutine
@@ -84,5 +90,26 @@ int copyReadWrite(int src, int dest) {
 }
 
 int copyMmap(int src, int dest) {
+	//obtain source file size
+	off_t size = lseek(src, 0, SEEK_END);
+	if (size == -1) return 6;
+	lseek(src, 0, SEEK_SET);
+	
+	//map source file onto memory
+	char* srcMap = mmap(0, size, PROT_READ, MAP_PRIVATE, src, 0);
+	if (srcMap == MAP_FAILED) return 7;
+	
+	//map destination file, ensure the file is long enough to store source
+	ftruncate(dest, size);
+	char* dstMap = mmap(0, size, PROT_WRITE, MAP_SHARED, dest, 0);
+	if (dstMap == MAP_FAILED) return 8;
+	
+	//copy memory
+	memcpy(dstMap, srcMap, size);
+	
+	//unmap memory
+	munmap(srcMap, size);
+	munmap(dstMap, size);
+	
 	return 0;
 }
